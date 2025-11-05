e600d957a1e41590b5e8f4fbddf00842595b68aa
Yogesh <dyogesh.official@gmail.com>
2025-11-05 17:38:41 +0530

    update

--- home/.config/mango/autostart.sh
+++ home/.config/mango/autostart.sh
@@ -3,3 +3,6 @@ swaybg -i Pictures/Wallpapers/default
 wl-paste --type text --watch cliphist store &
 wl-paste --type image --watch cliphist store & 
 swaync &
+swayidle \
+  timeout 600 'wlopm --off "*"' \
+  resume 'wlopm --on "*"'
