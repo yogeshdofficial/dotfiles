93c72150ad5a931be5a7e8736d9af162bdaa5e54
Yogesh <dyogesh.official@gmail.com>
2026-01-17 18:51:49 +0530

    updates

--- home/.config/mango/autostart.sh
+++ home/.config/mango/autostart.sh
@@ -4,6 +4,7 @@ wl-clip-persist --clipboard regular --reconnect-tries 0 &
 wl-paste --type text --watch cliphist store &
 wl-paste --type image --watch cliphist store & 
 swaync &
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
 #/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 #swayidle \
 #  timeout 600 'wlopm --off "*"' \
